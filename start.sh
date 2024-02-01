eca51ef12263d302a900cacc97cc4724329ce890
mongo928 <132264693+mongo928@users.noreply.github.com>
2024-02-01 21:47:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mongo928/test1.git /LazyPrincess
+  git clone https://github.com/mongo928/Lazyprincess /LazyPrincess
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO"
   git clone $UPSTREAM_REPO /LazyPrincess
-if
-  cd /LazyPrincess
-  pip3 install -U -r requirements.txt
-  echo "Starting Bot...." 
-  python3 bot.py
+fi
+cd /LazyPrincess
+pip3 install -U -r requirements.txt
+echo "Starting Bot...." 
+python3 bot.py
